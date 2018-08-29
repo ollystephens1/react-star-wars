ae92a378ba1dc966359cef90ce785ccb23fc38d7
ollystephens1 <ostephens88@hotmail.co.uk>
2018-08-29 10:58:19 +0200

    feat: header styles

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom'
 export default class Header extends React.Component {_
   render() {
     return (
-      <nav className="navbar navbar-light bg-light">
-        <Link to="/" className="navbar-brand">Star Wars</Link>
+      <nav className="navbar bg-dark text-center">
+        <Link to="/" className="navbar-brand">
+          <img src="assets/img/logo.png" className="imng-fluid" height="50" />
+        </Link>
       </nav>
     );
   }
